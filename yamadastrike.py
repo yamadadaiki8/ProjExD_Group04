5be8d2a06c0ba693913654c2daf567d89c6b9d0c
c0a2502654 <c0a2502654@edu.teu.ac.jp>
2026-05-19 14:35:29 +0900

    初期状態

---
+++ yamadastrike.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import sys
+import time
+import pygame as pg
+
+
+WIDTH = 450  # ゲームウィンドウの幅
+HEIGHT = 800  # ゲームウィンドウの高さ
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
